c18aa2890bc9f6cff063794d3a550f731da58ce8
Nishan <ncha0071@student.monash.edu>
2022-11-06 16:01:50 +1100

    drums: play by click

--- js/drums.js
+++ js/drums.js
@@ -1,18 +1,32 @@
 removeTransition = (e) => {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
 
-  playSound = (e) => {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
+playSound = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
 
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-  }
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
 
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+const keys = document.querySelectorAll('.key')
+Array.from(keys).forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+
+
+keys.forEach(key => key.addEventListener('click', () => {
+  console.log(key)
+
+  const dataKey = key.className.split(" ")[1]
+  // better
+
+  const audio = document.querySelector(`audio[data-key="${dataKey}"]`);
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}));
