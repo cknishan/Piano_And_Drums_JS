04539e570c7288cf8fd35a1e6ef695bfce2bdcad
Nishan <nishan.chakma.nish@gmail.com>
2022-11-08 23:05:44 +1100

    piano function added

--- js/piano.js
+++ js/piano.js
@@ -1,13 +1,14 @@
 const pianoKeys = document.querySelectorAll('.key')
 
+// some of the notes don't sound the same so had to break the serial
+const notes = ["e3","f3","f-3","g3","g-3","a4","a-4","b3","c4","c-4","d4","d-4","e4","f4","f-4","g4","g-4","a5","a-5","b5"]
+
 function playSound(newUrl) {
     console.log(newUrl)
     new Audio(newUrl).play()
 }
 
-
 pianoKeys.forEach((pianoKey, i) => {
-    const number = i < 9 ? '0' + (i + 1) : (i + 1)
-    const newUrl = '24-piano-keys/key' + number + '.mp3'
+    const newUrl = '../sounds/piano_sounds/' + notes[i] + '.mp3'
     pianoKey.addEventListener('click', () => playSound(newUrl))
 })
